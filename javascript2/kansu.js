e655044ba0ed59a9fcb32f33611a827938f40dd4
alessio80funky <alessio80funky@gmail.com>
2026-09-04 15:47:45 +0900

    9/4 15:47

--- javascript2/kansu.js
+++ javascript2/kansu.js
@@ -21,3 +21,41 @@ let Aisatsu = () =>{
 }
 
 Aisatsu();
+
+//引数
+
+///引数は関数を実行するときに渡すデータ
+
+
+let name = " taro ";
+let nation = "日本人";
+
+
+greet(name, nation);
+
+                //引数
+function greet(name,nation){
+   console.log("こんにちは" + name + "さん")
+   console.log(`私は${nation}です,こんにちは${name}さん`)
+   return;///一連の流れが終わったら振り出しにもどる
+};
+
+
+///戻り値(return)
+///関数の外にデータを返すための命令文です。
+///もし、データがなければ、振り出しにもどる
+
+let x = 4;
+let y = 2;
+
+console.log(add(x,y));
+
+function add(x,y){
+    return x * y //returnのあとにデータがあったら処理した状態で、関数の外に渡す。
+};
+
+
+
+
+
+
